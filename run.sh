6acc4a18419dd5c61c8a5182adfd9b92beefdfb0
Noah Streller <noah.streller@raiffeisen.ch>
2025-05-08 11:50:11 +0200

    fix(security): unset secret management credentials

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$INFISICAL_MACHINE_CLIENT_ID --client-secret=$INFISICAL_MACHINE_CLIENT_SECRET --plain --silent)
+INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$INFISICAL_MACHINE_CLIENT_ID --client-secret=$INFISICAL_MACHINE_CLIENT_SECRET --plain --silent)
+unset INFISICAL_MACHINE_CLIENT_ID
+unset INFISICAL_MACHINE_CLIENT_SECRET
 exec infisical run --token $INFISICAL_TOKEN --projectId $PROJECT_ID --env $INFISICAL_SECRET_ENV --domain $INFISICAL_API_URL -- npm start
