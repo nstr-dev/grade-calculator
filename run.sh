397e246764d58df95c9f3fd99fa7605835fba565
Noah Streller <noah.streller@raiffeisen.ch>
2025-05-08 11:55:28 +0200

    chore: undo setting next auth url because its not exported

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$INFISICAL_MACHINE_CLIENT_ID --client-secret=$INFISICAL_MACHINE_CLIENT_SECRET --plain --silent)
 unset INFISICAL_MACHINE_CLIENT_ID
 unset INFISICAL_MACHINE_CLIENT_SECRET
-export NEXTAUTH_URL=${NEXTAUTH_URL:-$COOLIFY_DOMAIN_URL}
 exec infisical run --token $INFISICAL_TOKEN --projectId $PROJECT_ID --env $INFISICAL_SECRET_ENV --domain $INFISICAL_API_URL -- npm start
